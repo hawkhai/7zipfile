b17587a13e9f4be7de1f7ed0ce55d00ce72a9c6d
haihoing <haihoing@qq.com>
2023-09-09 21:54:51 +0800

    清理无用的代码。

--- lzma2301/C/Util/7z/7zMainLib.c
+++ lzma2301/C/Util/7z/7zMainLib.c
@@ -310,13 +310,13 @@ static char* UIntToStr(char* s, unsigned value, int numDigits)
     *s = '\0';
     return s;
 }
-
+/**
 static void UIntToStr_2(char* s, unsigned value)
 {
     s[0] = (char)('0' + (value / 10));
     s[1] = (char)('0' + (value % 10));
 }
-
+*/
 #define PERIOD_4 (4 * 365 + 1)
 #define PERIOD_100 (PERIOD_4 * 25 - 1)
 #define PERIOD_400 (PERIOD_100 * 4 + 1)
@@ -427,13 +427,14 @@ static WRes Set_File_FILETIME(const wchar_t* name, const FILETIME* mTime)
 }
 
 #endif
-
+/**
 static void NtfsFileTime_to_FILETIME(const CNtfsFileTime* t, FILETIME* ft)
 {
     ft->dwLowDateTime = (DWORD)(t->Low);
     ft->dwHighDateTime = (DWORD)(t->High);
 }
-
+*/
+/**
 static void ConvertFileTimeToString(const CNtfsFileTime* nTime, char* s)
 {
     unsigned year, mon, hour, min, sec;
@@ -482,7 +483,7 @@ static void ConvertFileTimeToString(const CNtfsFileTime* nTime, char* s)
     UIntToStr_2(s, min); s[2] = ':'; s += 3;
     UIntToStr_2(s, sec); s[2] = 0;
 }
-
+*/
 static void PrintLF(void)
 {
     Print("\n");
@@ -531,7 +532,7 @@ static void PrintError_WRes(const char* message, WRes wres)
 #endif
     PrintLF();
 }
-
+/**
 static void GetAttribString(UInt32 wa, BoolInt isDir, char* s)
 {
 #ifdef USE_WINDOWS_FILE
@@ -546,278 +547,7 @@ static void GetAttribString(UInt32 wa, BoolInt isDir, char* s)
     s[1] = 0;
 #endif
 }
-
-/*
-    pSrcFile : .7z文件名（可包含路径）
-    pDstPath : 解压至目标文件夹（必须用绝对路径，目录必须存在，如果为空，直接解压到当前目录）
 */
-int /*MY_CDECL*/ extract_7z(const wchar_t* pSrcFile, const wchar_t* pDstPath) {
-    int useDestPath = 0;
-    wchar_t szDstPath[MAX_PATH] = { 0 };
-    if (pSrcFile == NULL)
-    {
-        return SZ_ERROR_FAIL;
-    }
-    if (pDstPath)
-    {
-        useDestPath = 1;
-        wcscpy_s(szDstPath, MAX_PATH, pDstPath);
-        pDstPath = szDstPath;
-        PathAddBackslash(szDstPath);
-        MyCreateDir(pDstPath);
-        if (!PathFileExists(pDstPath))
-        {
-            return SZ_ERROR_FAIL;
-        }
-    }
-    CFileInStream archiveStream;
-    CLookToRead2 lookStream;
-    CSzArEx db;
-    SRes res;
-    ISzAlloc allocImp;
-    ISzAlloc allocTempImp;
-    wchar_t* temp = NULL;
-    wchar_t* destPath = NULL;
-    size_t tempSize = 0;
-    size_t destSize = 0;
-    // UInt32 parents[NUM_PARENTS_MAX];
-
-    printf("\n7z ANSI-C Decoder " MY_VERSION_COPYRIGHT_DATE "\n\n");
-
-#if defined(_WIN32) && !defined(USE_WINDOWS_FILE) && !defined(UNDER_CE)
-    g_FileCodePage = AreFileApisANSI() ? CP_ACP : CP_OEMCP;
-#endif
-
-    allocImp.Alloc = SzAlloc;
-    allocImp.Free = SzFree;
-
-    allocTempImp.Alloc = SzAllocTemp;
-    allocTempImp.Free = SzFreeTemp;
-
-#ifdef UNDER_CE
-    if (InFile_OpenW(&archiveStream.file, L"\test.7z"))
-#else
-    if (InFile_OpenW(&archiveStream.file, pSrcFile))
-#endif
-    {
-        PrintError("can not open input file");
-        return SZ_ERROR_NO_ARCHIVE;
-    }
-
-    FileInStream_CreateVTable(&archiveStream);
-    LookToRead2_CreateVTable(&lookStream, False);
-
-    lookStream.realStream = &archiveStream.vt;
-    LookToRead2_INIT(&lookStream);
-
-    CrcGenerateTable();
-
-    SzArEx_Init(&db);
-
-    res = SzArEx_Open(&db, &lookStream.vt, &allocImp, &allocTempImp);
-
-    if (res == SZ_OK)
-    {
-        int listCommand = 0, testCommand = 0, fullPaths = 1;
-
-        if (res == SZ_OK)
-        {
-            UInt32 i;
-
-            /*
-            if you need cache, use these 3 variables.
-            if you use external function, you can make these variable as static.
-            */
-            UInt32 blockIndex = 0xFFFFFFFF; /* it can have any value before first call (if outBuffer = 0) */
-            Byte* outBuffer = 0; /* it must be 0 before first call for each new archive. */
-            size_t outBufferSize = 0;  /* it can have any value before first call (if outBuffer = 0) */
-
-            for (i = 0; i < db.NumFiles; i++)
-            {
-                size_t offset = 0;
-                size_t outSizeProcessed = 0;
-                // const CSzFileItem *f = db.Files + i;
-                size_t len;
-                unsigned isDir = SzArEx_IsDir(&db, i);
-                if (listCommand == 0 && isDir && !fullPaths)
-                    continue;
-                len = SzArEx_GetFileNameUtf16(&db, i, NULL);
-                // len = SzArEx_GetFullNameLen(&db, i);
-
-                if (len > tempSize)
-                {
-                    SzFree(NULL, temp);
-                    tempSize = len;
-                    temp = (wchar_t*)SzAlloc(NULL, (tempSize + 1) * sizeof(wchar_t));
-                    if (!temp)
-                    {
-                        res = SZ_ERROR_MEM;
-                        break;
-                    }
-                }
-
-                SzArEx_GetFileNameUtf16(&db, i, temp);
-                /*
-                if (SzArEx_GetFullNameUtf16_Back(&db, i, temp + len) != temp)
-                {
-                res = SZ_ERROR_FAIL;
-                break;
-                }
-                */
-
-                if (listCommand)
-                {
-                    char attr[8], s[32], t[32];
-                    UInt64 fileSize;
-
-                    GetAttribString(SzBitWithVals_Check(&db.Attribs, i) ? db.Attribs.Vals[i] : 0, isDir, attr);
-
-                    fileSize = SzArEx_GetFileSize(&db, i);
-                    UInt64ToStr(fileSize, s, 10);
-
-                    if (SzBitWithVals_Check(&db.MTime, i))
-                        ConvertFileTimeToString(&db.MTime.Vals[i], t);
-                    else
-                    {
-                        size_t j;
-                        for (j = 0; j < 19; j++)
-                            t[j] = ' ';
-                        t[j] = '\0';
-                    }
-
-                    printf("%s %s %10s  ", t, attr, s);
-                    res = PrintString(temp);
-                    if (res != SZ_OK)
-                        break;
-                    if (isDir)
-                        printf("/");
-                    printf("\n");
-                    continue;
-                }
-
-                fputs(testCommand ?
-                    "Testing    " :
-                    "Extracting ",
-                    stdout);
-                res = PrintString(temp);
-                if (res != SZ_OK)
-                    break;
-
-                if (isDir)
-                    printf("/");
-                else
-                {
-                    res = SzArEx_Extract(&db, &lookStream.vt, i,
-                        &blockIndex, &outBuffer, &outBufferSize,
-                        &offset, &outSizeProcessed,
-                        &allocImp, &allocTempImp);
-                    if (res != SZ_OK)
-                        break;
-                }
-
-                if (!testCommand)
-                {
-                    CSzFile outFile;
-                    size_t processedSize;
-                    size_t j;
-                    wchar_t* name = (wchar_t*)temp;
-                    if (useDestPath) {
-                        SzFree(NULL, destPath);
-                        destSize = wcslen(pDstPath);
-                        destSize += tempSize;
-                        destPath = (wchar_t*)SzAlloc(NULL, (destSize + 1) * sizeof(wchar_t));
-                        if (!destPath)
-                        {
-                            res = SZ_ERROR_MEM;
-                            break;
-                        }
-                        wcscpy_s(destPath, destSize + 1, pDstPath);
-                        wcscat_s(destPath, destSize + 1, temp);
-                        name = destPath + wcslen(pDstPath);
-                    }
-                    else
-                        destPath = (wchar_t*)name;
-
-
-                    for (j = 0; name[j] != 0; j++)
-                        if (name[j] == '/')
-                        {
-                            if (fullPaths)
-                            {
-                                name[j] = 0;
-                                MyCreateDir(destPath);
-                                name[j] = CHAR_PATH_SEPARATOR;
-                            }
-                            else
-                                destPath = name + j + 1;
-                        }
-
-                    if (isDir)
-                    {
-                        MyCreateDir(destPath);
-                        printf("\n");
-                        continue;
-                    }
-                    else if (OutFile_OpenUtf16(&outFile, destPath))
-                    {
-                        PrintError("can not open output file");
-                        res = SZ_ERROR_FAIL;
-                        break;
-                    }
-
-                    processedSize = outSizeProcessed;
-
-                    if (File_Write(&outFile, outBuffer + offset, &processedSize) != 0 || processedSize != outSizeProcessed)
-                    {
-                        PrintError("can not write output file");
-                        res = SZ_ERROR_FAIL;
-                        break;
-                    }
-
-                    if (File_Close(&outFile))
-                    {
-                        PrintError("can not close output file");
-                        res = SZ_ERROR_FAIL;
-                        break;
-                    }
-
-#ifdef USE_WINDOWS_FILE
-                    if (SzBitWithVals_Check(&db.Attribs, i))
-                        SetFileAttributesW(destPath, db.Attribs.Vals[i]);
-#endif              
-                }
-                printf("\n");
-            }
-            IAlloc_Free(&allocImp, outBuffer);
-        }
-    }
-
-    SzArEx_Free(&db, &allocImp);
-    SzFree(NULL, temp);
-    if (useDestPath) {
-        SzFree(NULL, destPath);
-    }
-
-    File_Close(&archiveStream.file);
-
-    if (res == SZ_OK)
-    {
-        printf("\nEverything is Ok\n");
-        return res;
-    }
-
-    if (res == SZ_ERROR_UNSUPPORTED)
-        PrintError("decoder doesn't support this archive");
-    else if (res == SZ_ERROR_MEM)
-        PrintError("can not allocate memory");
-    else if (res == SZ_ERROR_CRC)
-        PrintError("CRC error");
-    else
-        printf("\nERROR #%d\n", res);
-
-    return res;
-}
-
 // #define NUM_PARENTS_MAX 128
 
 //int Z7_CDECL main(int numargs, char *args[])
@@ -827,10 +557,6 @@ int /*MY_CDECL*/ extract_7z(const wchar_t* pSrcFile, const wchar_t* pDstPath) {
     */
 int Z7DLLEXPORT Extract7z(const wchar_t* pSrcFile, const wchar_t* pDstPath)
 {
-    //if (pSrcFile) {
-    //    return extract_7z(pSrcFile, pDstPath);
-    //}
-
     int useDestPath = 0;
     wchar_t szDstPath[MAX_PATH] = { 0 };
 
@@ -1015,11 +741,17 @@ int Z7DLLEXPORT Extract7z(const wchar_t* pSrcFile, const wchar_t* pDstPath)
                         if (fullPaths)
                         {
                             name[j] = 0;
-                            MyCreateDir(name);
+                            if (useDestPath) {
+                                MyCreateDir(destPathPtr);
+                            }
+                            else {
+                                MyCreateDir(name);
+                            }
                             name[j] = CHAR_PATH_SEPARATOR;
                         }
-                        else
+                        else {
                             destPathPtr = name + j + 1;
+                        }
                     }
                 }
 
